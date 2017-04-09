fa5bd99f940ead7d3fa88ce76f6ce16c588c01ac
winnerzaza <chollachart.win@mail.kmutt.ac.th>
2017-04-09 12:18:23 +0700

    Add files via upload

---
+++ TestTimer.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bike.timer;
+
+/**
+ *
+ * @author user
+ */
+public class TestTimer {
+    public static void main(String[] args) {
+        Timer t = new Timer();
+        t.showStartAndEndTime();
+        System.out.println(t.getshowStartAndEndTime());
+    }
+}
