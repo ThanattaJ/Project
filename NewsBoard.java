cacd6748444bbd3d16929cb814d1e4549017b477
winnerzaza <chollachart.win@mail.kmutt.ac.th>
2017-04-19 13:57:15 +0700

    Add files via upload

--- NewsBoard.java
+++ NewsBoard.java
@@ -3,13 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package int105.news;
+package bike.news;
 
 /**
  *
  * @author user
  */
-import int105.news.Database;
+import bike.database.Database;
+//import int105.profile.UserProfile;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -20,10 +21,14 @@ import java.sql.Statement;
 public class NewsBoard {
     private String nameNews;
     private String descrip;
-
+//    private UserProfile up = new UserProfile();
     public NewsBoard(String nameNews, String descrip) {
         this.nameNews = nameNews;
         this.descrip = descrip;
+    }
+    
+    public NewsBoard(){
+        
     }
 
     public String getNameNews() {
@@ -49,16 +54,7 @@ public class NewsBoard {
             System.out.println("Database connecting");
         
             Statement st = connect.createStatement(); 
-            String temp = "SELECT * FROM GreenSociety.News";
-            
-            ResultSet rs = st.executeQuery(temp);
-            
-//            while(rs.next()){
-//                System.out.println("newsID: " + rs.getInt("newsID"));
-//                System.out.println("newsDescription: " + rs.getString("newsDescription"));
-//                System.out.println("newslDetails: " + rs.getString("manualDetails"));
-//                System.out.println("----------------------------------------------");
-//            }
+            String temp = "SELECT * FROM News";
             
             try {
 		if(connect != null){
@@ -76,6 +72,44 @@ public class NewsBoard {
             System.out.println(ex);
         }
         System.out.println("");
+    }
+    public void getNews(int newsID){
+        try{
+            Database cndb = new Database();
+            Connection connect = Database.connectDb("win", "win016");
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Database connecting");
+        
+            Statement st = connect.createStatement(); 
+            String temp = "SELECT * FROM News WHERE newsID="+newsID;
+            ResultSet rs = st.executeQuery(temp);
+            
+            st = connect.createStatement();
+            String temp2 = "INSERT into News_Reader (newsID, userID) VALUES (12345,12345)";
+            st.executeUpdate(temp2);
+    
+            while(rs.next()){
+                System.out.println("newsID: " + rs.getInt("newsID"));
+                System.out.println("newsDescription: " + rs.getString("newsDescription"));
+                System.out.println("newslDetails: " + rs.getString("newsDetails"));
+                System.out.println("----------------------------------------------");
+            }
+            
+            try {
+		if(connect != null){
+                    connect.close();
+		}
+		}catch (SQLException e){
+                    e.printStackTrace();
+		}
+        }
+        
+        catch(ClassNotFoundException cfe){
+            System.out.println(cfe);
+        }
+        catch(Exception ex){
+            System.out.println(ex);
+        }
     }
     public void showNews(){
         try{
@@ -113,17 +147,21 @@ public class NewsBoard {
         System.out.println("");
                                  
     }
+    public void insertData() {
+        Connection c = null;
+        try {
+            c = Database.connectDb("win", "win016");
+            Statement s = c.createStatement();
+            String sql = "INSERT into News_Reader VALUES userID,newsID='";
+            s.executeUpdate(sql);
+        } catch (Exception e) {
+            e.printStackTrace();    //โชว์ข้อผิดพลาดทั้งหมด
+        }
+        try {
+            c.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
 }
-//    public int showNews(int listNews){
-//        switch(listNews){
-//            case 0:     
-//                System.out.println("Latest News");
-//                break;
-//            case 1:
-//                System.out.println("All News");
-//                
-//                break;
-//            
-//        }
-//        return listNews;
-//    }
